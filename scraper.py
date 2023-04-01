9dce1adfdd9713722f20c059563a7027aa2e8ae2
banditt37 <kamilstefanski00@gmail.com>
2023-04-01 10:41:23 +0200

    2023-04-01 KS

--- scraper.py
+++ scraper.py
@@ -1,8 +1,34 @@
+import json
 import requests
+from bs4 import BeautifulSoup
+
+def get_element():
+    pass
+
 # product_code = input("Podaj kod produktu: ")
 product_code = "100361771"
 
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 
 response = requests.get(url)
-print(response.status_code)
+if response.status_code == requests.codes.ok:
+    page_dom = BeautifulSoup(response.text, "html.parser")
+    opinions = page_dom.select("div.js_product-review")
+    opinions_all = []
+    for opinion in opinions:
+        single_opinion = {
+            "opinion_id": opinion("data-entry-id"),
+            "author": opinion.select_one("span. user-post_author-name").getText().strip(),
+            "recommendation": opinion.select_one("span.user-post_author-recomendation").getText().strip(),
+            "score": opinion.select_one("span.user-post_score-count").getText().strip(),
+            "confirmed": opinion.select_one("div.review-pz").getText().strip(),
+            "opinion_date": opinion.select_one("span.user-post_published > time:nth-child(1)["datetime"]").getText().strip(),
+            "purchase_date": opinion.select_one("span.user-post_published > time:nth-child(2)["datetime"]").getText().strip(),
+            "up_votes": opinion.select_one("span[id^="votes-yes"]").getText().strip(),
+            "down_votes": opinion.select_one("span[id^="votes-no"]").getText().strip(),
+            "content": opinion.select_one("div.user-post_text").getText().strip(),
+            "cons": [p.get_text().strip() for p in opinion.select("div.review-feature_col:has(>div.review-feature__title--negatives) > div.review-feature__item")],
+            "pros": [p.get_text().strip() for p in opinion.select("div.review-feature_col:has(>div.review-feature__title--negatives) > div.review-feature__item")]
+        }
+
+    print(json.dumps(single_opinion,indent=4, ensure_ascii=False))
